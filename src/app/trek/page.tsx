7629eacb6ff662c281fd055aca33856ffe8ff1a0
aloknegi786 <aloksinghnegi005@gmail.com>
2026-01-22 21:27:38 +0530

    trek page created

--- src/app/trek/page.tsx
+++ src/app/trek/page.tsx
@@ -1,8 +1,33 @@
+import {
+  TrekHero,
+  TrekMetaBar,
+  TrekOverview,
+  TrekInclusions,
+  TrekGallery,
+  TrekBookingCard,
+  TrekEnquiryForm
+} from "@/components/trek"
+import { TrekItinerary } from "@/components/trek/TrekItinerary"
 
-export default function trekpage() {
+export default function TrekDetailPage() {
   return (
-    <div >
-      treks details 
-    </div>
-  );
+    <>
+      <TrekHero />
+      <TrekMetaBar />
+
+      <main className="max-w-6xl mx-auto px-4 py-10 grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div className="lg:col-span-2 space-y-8">
+          <TrekOverview />
+          {/* <TrekInclusions /> */}
+          <TrekItinerary />
+          <TrekGallery />
+        </div>
+
+        <div className="lg:block sticky top-24 h-fit space-y-6">
+          <TrekBookingCard />
+          <TrekEnquiryForm />
+        </div>
+      </main>
+    </>
+  )
 }
